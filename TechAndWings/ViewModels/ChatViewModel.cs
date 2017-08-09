9f912e613d9ecb1139f5c1a33cf11aa1419932e0
jeffbmiller <jeffmiller11@gmail.com>
2017-08-09 18:48:58 -0500

    Add Message input

--- TechAndWings/ViewModels/ChatViewModel.cs
+++ TechAndWings/ViewModels/ChatViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using TechAndWings.Models;
+using Xamarin.Forms;
 
 namespace TechAndWings.ViewModels
 {
@@ -16,8 +17,24 @@ namespace TechAndWings.ViewModels
             Messages.Add(new MessageViewModel(new Message() { Text = "Where do we want to meet this month?", User = "Jeff" }, user));
             Messages.Add(new MessageViewModel(new Message() { Text = "How about The Decker?", User = "James" }, user));
             Messages.Add(new MessageViewModel(new Message() { Text = "Sounds good to me.", User = "Jeff" }, user));
+
+            SendMessageCommand = new Command(() => OnSendMessage(), () => Message != null);
         }
 
         public ObservableCollection<MessageViewModel> Messages { get; set; }
+
+		string message = string.Empty;
+		public string Message
+		{
+			get { return message; }
+			set { message = value; OnPropertyChanged(); }
+		}
+
+        public Command SendMessageCommand { get; set; }
+
+        private void OnSendMessage(){
+            Messages.Add(new MessageViewModel(new Models.Message(){Text = Message, User=user},user));
+            Message = null;
+        }
     }
 }
